8c46fe9a85485adae488b0d06a33eea5aeb4fded
Luc Mcgrady <lucmcgrady@gmail.com>
2025-08-14 00:38:15 +0100

    Print text even if tts fails

--- src/index.ts
+++ src/index.ts
@@ -31,12 +31,12 @@ async function go() {
     source.connect(audioCtx.destination)
 
     const text = getPlainTextFromGameMessage()
+    console.log(text)
     
     const { audio } = await synthesize({text, voice: short_name, language: locale})
     const audioBuffer = await audio.arrayBuffer()
     source.buffer = await audioCtx.decodeAudioData(audioBuffer)
     source.start()
-    console.log(text)
 }
 
 const element = document.createElement("button")
